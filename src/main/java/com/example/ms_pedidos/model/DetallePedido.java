2d6d8bd358fbb589dec7eb91b9f34eb90b6ae7ec
dxminique <do.cofre@duocuc.cl>
2025-11-18 16:06:07 -0300

    Agregar modelos Pedido y DetallePedido

---
+++ src/main/java/com/example/ms_pedidos/model/DetallePedido.java
@@ -0,0 +1,19 @@
+package com.example.ms_pedidos.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class DetallePedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long idProducto;
+    private Integer cantidad;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+}
---
+++ src/main/java/com/example/ms_pedidos/model/Pedido.java
@@ -0,0 +1,19 @@
+package com.example.ms_pedidos.model;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.List;
+
+@Entity
+@Data
+public class Pedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fecha;
+    private Long idUsuario;
+
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
+    private List<DetallePedido> detalles;
+}
